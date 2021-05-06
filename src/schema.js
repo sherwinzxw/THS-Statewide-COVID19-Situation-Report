4a664cb957a87aad7fcaa7443ba69b7013076c88
Alastair Taft <AlastairTaft@users.noreply.github.com>
2021-05-06 15:59:00 +1000

    Added test input

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,49 @@
-export default {
+{
   "schemaVersion": 5,
-  "layout": []
+  "layout": [
+    {
+      "type": "page",
+      "layout": [
+        {
+          "type": "form",
+          "layout": [
+            {
+              "type": "unstyled",
+              "key": "g9zzng",
+              "text": ""
+            },
+            {
+              "text": " ",
+              "type": "text",
+              "key": "1vbpa",
+              "header": "Input 1"
+            },
+            {
+              "text": "",
+              "type": "unstyled",
+              "key": "3bmk8"
+            }
+          ],
+          "key": "tycc6n"
+        }
+      ],
+      "key": "uqr76j"
+    },
+    {
+      "type": "page",
+      "layout": [
+        {
+          "type": "form",
+          "layout": [
+            {
+              "type": "unstyled",
+              "key": "t4vgxm"
+            }
+          ],
+          "key": "zb7hhy"
+        }
+      ],
+      "key": "fyyen2"
+    }
+  ]
 }
