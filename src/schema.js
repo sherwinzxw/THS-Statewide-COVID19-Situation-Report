a2dc34b40a905387dfb797d6080e2380543bec82
Alastair Taft <AlastairTaft@users.noreply.github.com>
2021-05-06 16:03:56 +1000

    Test adding content to schema

--- src/schema.js
+++ src/schema.js
@@ -14,14 +14,31 @@
             },
             {
               "text": " ",
-              "type": "text",
+              "type": "checkbox",
               "key": "1vbpa",
-              "header": "Input 1"
+              "header": "Input 1",
+              "placeholder": "qweqwe",
+              "helpText": "qewqweq"
+            },
+            {
+              "text": "qweqeq",
+              "type": "unstyled",
+              "key": "3bmk8"
             },
             {
               "text": "",
               "type": "unstyled",
-              "key": "3bmk8"
+              "key": "a68ll"
+            },
+            {
+              "text": "qweqweqwe",
+              "type": "header-two",
+              "key": "24jlf"
+            },
+            {
+              "key": "ypg8un",
+              "type": "unstyled",
+              "text": " "
             }
           ],
           "key": "tycc6n"
