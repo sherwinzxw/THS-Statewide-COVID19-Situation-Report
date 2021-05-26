3f1961a7a29812f3def68734f17e68d99226b87e
Sherwin <12377619+sherwinzxw@users.noreply.github.com>
2021-05-26 11:58:54 +1000

    Add new scenes component via upload
    Add new scenes component RespiratoryClinicAppointmentsTable via file upload

---
+++ src/scenes/RespiratoryClinicAppointmentsTable/index.js
@@ -0,0 +1,93 @@
+import * as React from 'react'
+import { EditableTable as Table } from './../../components'
+
+const { Fragment, useState, useEffect } = React
+
+/**
+ * These are the specific controls this component will render.
+ */
+export const controlMap = {
+    'Control_3FD4CD18-44EB-4308-990A-2363BDA0DA91':'Melville Street',
+    'Control_c2faaba1-da7a-4bdb-9a01-af7a851529df':'East Devonport',
+    'Control_AAD8C6A2-8293-4029-A950-DD0059FCFC8A':'Wellington Street',
+}
+
+
+/**
+ * @property {object} props.value This is a hash map of what the original 
+ * control id was to the value.
+ */
+const RespiratoryClinicAppointmentsTable = props => {
+
+  const { 
+    value: defaultValue, 
+    onChangeValue: parentOnChangeValue,
+    errorMessage,
+  } = props
+
+  const [value, setValue] = useState(defaultValue)
+  const onChangeValue = val => {
+    setValue(val)
+    parentOnChangeValue(val)
+  }
+
+  useEffect(() => {
+    setValue(defaultValue)
+  }, [defaultValue])
+
+  return <Table 
+    value={value} 
+    onChangeValue={onChangeValue}
+    errorMessage={errorMessage}
+    controlMap={controlMap}
+  >
+    {({ renderCellError, renderCellInput }) => <Fragment>
+      <thead>
+        <tr className="header-one">
+          <th>Location</th>
+          <th>Booked</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td rowSpan={2}>Melville Street</td>
+          {renderCellInput(
+            'Melville Street',
+            { contentEditable: true, },
+          )}
+        </tr>
+        <tr>
+          {renderCellError(
+            'Melville Street',
+          )}
+        </tr>        
+        <tr>
+          <td rowSpan={2}>Wellington Street</td>
+          {renderCellInput(
+            'Wellington Street', 
+            { contentEditable: true, },
+          )}
+        </tr>
+        <tr>
+          {renderCellError(
+            'Wellington Street',
+          )}
+        </tr>
+        <tr>
+          <td rowSpan={2}>East Devonport</td>
+          {renderCellInput(
+            'East Devonport', 
+            { contentEditable: true, },
+          )}
+        </tr>
+        <tr>
+          {renderCellError(
+            'East Devonport',
+          )}
+        </tr>       
+      </tbody>
+    </Fragment>}
+  </Table>
+}
+
+export default RespiratoryClinicAppointmentsTable
