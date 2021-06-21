a7f81ad2296a8ff1f422580183b57d1252043fe7
Sherwin <sherwinzxw1990@gmail.com>
2021-06-21 15:46:00 +1000

    Updated controMap.js
    Update the control mapping to include more locations

--- src/tables/RespiratoryClinicPresentationsTable/controlMap.js
+++ src/tables/RespiratoryClinicPresentationsTable/controlMap.js
@@ -5,7 +5,17 @@ const controlMap = {
   'Control_4E25CB83-86F9-4C50-B3D5-F588BA2689A5': 'East Devonport',
   'Control_03B1B8AC-DA23-4BCB-89CA-643D50F3DB99': 'Wellington St Launceston',
   'Control_24CE73FA-EC70-4F34-A0F7-5497DFFE21FF': 'Portside Burnie',
-  'Control_28CCDC97-D21C-42C8-AE29-AE1A5C21D30E': 'Ibis Hotel',
+  'Control_28CCDC97-D21C-42C8-AE29-AE1A5C21D30E': 'Ibis Hotel',  
+  'Control_848C5C96-2FB0-4702-92D8-CA6D2361ABD1': 'Ibis Hotel - Staff',
+  'Control_1D935A10-6A5E-46C0-926E-6A7377888374': 'Best Western Hotel - Guest',
+  'Control_06FE4E1E-BAC8-4571-A8D2-62DA8444C218': 'Best Western Hotel - Staff',
+  'Control_CAC13318-91ED-4E3A-B715-5AFFBF00F46D': 'Hotel Peppers Seaport',
+  'Control_826AFA11-02DE-494B-B482-E0060682A654': 'Travel Lodge - Airport',
+  'Control_AA003735-BF4A-4389-8BEA-C9785815FC27': 'Travel Lodge - Hobart',
+  'Control_7851FB7D-1B2B-4071-AA8E-45DFF7B9C440': 'Wrest Point',
+  'Control_FDF53DF9-2BAC-43D0-B717-9A017EB7E75A': 'Sunrise Hotel',  
+  'Control_66DDF1BE-E90B-4E24-B56A-8B5E31D4D838': 'Hobart Airport',
+  'Control_0A1C5FCE-0B5B-4D69-B8C2-F95A9247BB1C': 'Hobart Port',
 }
 
 export default controlMap
